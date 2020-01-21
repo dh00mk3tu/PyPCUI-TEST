2397356478ada23906f7532eb1e68139af26b83c
dh00mk3tu <dh00mk3tu@users.noreply.github.com>
2020-01-21 16:37:45 +0530

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -8,19 +8,32 @@ frame.title("Translator Test ")
 frame.geometry("370x100")
 
 def trans() :
+def trans_fr() :
     input = entry.get()
     translator = Translator(service_urls=["translate.google.com"])
     translation = translator.translate(input, dest="fr")
-    Tranlated_Text = tk.Label(framae, text=f'Translation : {translation.text}', bg='green')
-    TRanslated_Text.grid(row=1, column=1)
+    Translated_Text = tk.Label(frame, text=f"Translation fr : {translation.text}", bg='green')
+    display = tk.Entry(frame)
+    Translated_Text.grid(row=1, column=1)
 
-label = tk.Label(frame, text="Text to  Translate :")
+def trans_ru() :
+    input = entry.get()
+    translator = Translator(service_urls=["translate.google.com"])
+    translation = translator.translate(input, dest="ru")
+    Translated_Text = tk.Label(frame, text=f"Translation ru : {translation.text}", bg='yellow')
+    display = tk.Entry(frame)
+    Translated_Text.grid(row=2, column=1)
+
+label = tk.Label(frame, text="Text to Translate :")
 label.grid(row=0, column=0)
 
 entry = tk.Entry(frame)
 entry.grid(row=0,column=1)
 
-button = tk.Button(frame, text = "Translate", command=trans)
-button.grid(row=0, column=2)
+btn_french = tk.Button(frame, text = "Translate to French", command=trans_fr)
+btn_french.grid(row=0, column=2)
+
+btn_ru = tk.Button(frame, text = "Translate to Russian", command=trans_ru)
+btn_ru.grid(row=0, column=3)
 
 frame.mainloop()
